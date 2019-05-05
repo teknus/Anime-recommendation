aa6a64eba8907304b19ac064f975ad4b8aeaae69
Mateus Oliveira <devmateusoliveira@gmail.com>
2019-05-05 18:07:25 -0300

    ui scaffold

---
+++ src/pages/Suggestions/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import "./styles.css"
+export class Suggestions extends Component {
+	constructor(props){
+		super(props);
+		this.state = {suggestions:[]};
+	}
+	componentWillMount(){
+	//await api response to customer call
+	}
+	render() {
+		return (
+			<div className="page">
+			<div className="nav">
+				<h2>Minhas sugestões</h2>
+			</div>
+			<hr></hr>
+			<div className = "gallery">
+				<div className="suggestion">
+					<p className="description">Puesto de tacos</p>
+				</div>
+
+				<div className="suggestion">
+					<p className="description">Little pizza Emilio Portes Gil</p>
+				</div>
+
+
+				<div className="suggestion">
+					<p className="description">Restaurant los Compadres</p>
+				</div>
+
+
+				<div className="suggestion">
+					<p className="description">Taqueria EL amigo </p>
+				</div>
+			</div>
+			</div>
+		);
+	}
+}
